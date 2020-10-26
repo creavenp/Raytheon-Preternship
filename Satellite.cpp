f701231994847d862f06db0b611d4d7fb21f20a0
justinpajak <jpajak@nd.edu>
2020-10-26 09:21:35 -0400

    Segemented Satellite code into .cpp and .h

--- Satellite.cpp
+++ Satellite.cpp
@@ -1,37 +1,4 @@
-#include <iostream>
-
-class Satellite {
-
-     private:
-
-          // Coordinates of the satellite in outer space
-          // Relative to the center of the Earth
-          double x;
-          double y;
-          double z;
-
-     public:
-
-          Satellite(double x, double y, double z) : x(x), y(y), z(z) {}
-
-          double get_x() {
-               return x;
-          }
-
-          double get_y() {
-               return y;
-          }
-
-          double get_z() {
-               return z;
-          }
-
-          void set_coords(double x, double y, double z) {
-               this -> x = x;
-               this -> y = y;
-               this -> z = z;
-          }
-};
+#include "Satellite.h"
 
 int main() {
 
